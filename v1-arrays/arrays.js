40f1f1cf7238f18c9f238d60d3a09b9099b14bac
Chris Hoard <hoard.chris@gmail.com>
2017-07-15 14:22:01 +0000

    Finished V1

--- v1-arrays/arrays.js
+++ v1-arrays/arrays.js
@@ -1,1 +1,13 @@
+//V1 - Arrays
 var todos = ['item 1', 'item 2', 'item 3'];
+
+console.log('My Todos:',todos);
+
+todos.push('item 4');
+console.log('My Todos:',todos);
+
+todos[0] = 'item 1 updated';
+console.log('My Todos:',todos);
+
+todos.splice(3,1);
+console.log('My Todos:',todos);
