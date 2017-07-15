744ccc610d4dab579fe5266659454bfba4c769b8
Chris Hoard <hoard.chris@gmail.com>
2017-07-15 15:05:23 +0000

    Finished V3

--- v1-arrays/arrays.js
+++ v1-arrays/arrays.js
@@ -1,12 +1,12 @@
 //V1 - Arrays
-var todos = ['item 1', 'item 2', 'item 3'];
+//var todos = ['item 1', 'item 2', 'item 3'];
 
-displayTodos();
+//displayTodos();
 
-addTodos("item 4");
+//addTodos("item 4");
 
-changeTodos(0,'item 1 updated');
-removeTodos(3);
+//changeTodos(0,'item 1 updated');
+//removeTodos(3);
 
 //todos.push('item 4');
 //displayTodos();
@@ -17,7 +17,7 @@ removeTodos(3);
 //todos.splice(3,1);
 //displayTodos();
 //V2 Functions
-
+/*
 function displayTodos(){
     console.log('My Todos:',todos);
 }
@@ -36,12 +36,30 @@ function removeTodos(position){
     todos.splice(position,1);
     displayTodos();
 }
-
+*/
 //V3- Objects
-var myComputer = {
-    operatingSystem: "PC",
-    screenSize: "15 Inches",
-    purchaseYear: 2015
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+    displayTodos: function(){
+        console.log(this.todos);
+    },
+    addTodos: function(addItem){
+         this.push(addItem);
+         this.displayTodos();
+    },
+    changeTodos: function(position,newValue){
+         this.todos[position] = newValue;
+         this.displayTodos();
+    },
+    removeTodos: function(position){
+        this.splice(position,1);
+        this.displayTodos();
+    }
+    
 };
 
-console.log(myComputer.screenSize);
+todoList.displayTodos();
+todoList.addTodos("item 4");
+todoList.changeTodos(0,'item 1 updated');
+todoList.removeTodos(3);
+
